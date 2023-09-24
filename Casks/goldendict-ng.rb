585ae4103193aaa921d8b7ce8a502b4e264db13f
hantang <hantang@ht.dev>
2023-09-24 09:40:31 +0800

    update

--- Casks/goldendict-ng.rb
+++ Casks/goldendict-ng.rb
@@ -1,27 +1,27 @@
 cask "goldendict-ng" do
-    version "v23.09.10-WhiteDew.54c8bd56"
-    sha256 "879b64bac876d33c3408699ea1b7ed0df6a6a6d425301a98a97d6934d5799e65"
+  version "v23.09.10-WhiteDew.54c8bd56"
+  sha256 "879b64bac876d33c3408699ea1b7ed0df6a6a6d425301a98a97d6934d5799e65"
   
-    url "https://github.com/xiaoyifang/goldendict-ng/releases/download/#{version}/6.4.3-GoldenDict_macos-13_homebrew_20230910.dmg",
-        verified: "https://github.com/xiaoyifang/goldendict-ng/"
-    name "GoldenDict-ng"
-    desc "The Next Generation GoldenDict. A feature-rich open-source dictionary lookup program, supporting multiple dictionary formats and online dictionaries."
-    homepage "https://xiaoyifang.github.io/goldendict-ng/"
+  url "https://github.com/xiaoyifang/goldendict-ng/releases/download/#{version}/6.4.3-GoldenDict_macos-13_homebrew_20230910.dmg",
+    verified: "https://github.com/xiaoyifang/goldendict-ng/"
+  name "GoldenDict-ng"
+  desc "The Next Generation GoldenDict. A feature-rich open-source dictionary lookup program, supporting multiple dictionary formats and online dictionaries."
+  homepage "https://xiaoyifang.github.io/goldendict-ng/"
   
-    livecheck do
-      url :url # "https://github.com/xiaoyifang/goldendict-ng"
-      strategy :github_latest
-    end
-
-    # depends_on macos: ">= :sierra"
-    auto_updates true
-
-    app "GoldenDict.app"
-    # binary "#{appdir}/GoldenDict.app/Contents/MacOS/GoldenDict"
-  
-    zap trash: [
-      "~/Library/Preferences/org.xiaoyifang.plist",
-      "~/Library/Saved Application State/org.xiaoyifang.savedState",
-      "~/.goldendict",
-    ]
+  livecheck do
+    url :url # "https://github.com/xiaoyifang/goldendict-ng"
+    strategy :github_latest
   end
+
+  # depends_on macos: ">= :sierra"
+  auto_updates true
+
+  app "GoldenDict.app"
+  # binary "#{appdir}/GoldenDict.app/Contents/MacOS/GoldenDict"
+  
+  zap trash: [
+    "~/Library/Preferences/org.xiaoyifang.plist",
+    "~/Library/Saved Application State/org.xiaoyifang.savedState",
+    "~/.goldendict",
+  ]
+end

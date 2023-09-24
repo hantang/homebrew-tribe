b1d87bf8bdf82675a404593de43adda3088bdf10
hantang <hantang@ht.dev>
2023-09-24 10:00:15 +0800

    Update

--- Casks/squirreldisk.rb
+++ Casks/squirreldisk.rb
@@ -3,7 +3,7 @@ cask "squirreldisk" do
   sha256 "45820b944d22c6190db622c214800e81496ee8f8b148557015a44e6e0f245b49"
   
   url "https://github.com/adileo/squirreldisk/releases/download/v#{version}/SquirrelDisk_#{version}_x64.dmg"
-    verified: "https://github.com/adileo/squirreldisk/"
+    # verified: "https://github.com/adileo/squirreldisk/"
 
   name "SquirrelDisk"
   desc "Beautiful, Cross-Platform and Super Fast Disk Usage Analysis Tool - Built With Rust 🦀 "
@@ -18,6 +18,9 @@ cask "squirreldisk" do
 
   app "SquirrelDisk.app"
   
-  # zap trash: [
-  # ]
+  zap trash: [
+    "~/Library/Preferences/com.squirreldisk.dev.plist",
+    "~/Library/Saved Application State/com.squirreldisk.dev.savedState"
+    "~/Library/Caches/com.squirreldisk.dev"
+  ]
 end
